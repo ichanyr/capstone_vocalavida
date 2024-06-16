09a4c9419f23cbaedc858392659c6fb41e1338a7
syawalamir <syawalamir24@gmail.com>
2024-06-16 16:52:24 +0700

    penambahan soal, controller dan desain ui

--- lib/app/modules/latihan_detail/controllers/latihan_detail_controller.dart
+++ lib/app/modules/latihan_detail/controllers/latihan_detail_controller.dart
@@ -1,20 +1,145 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:capstone_vocalavida/app/routes/app_pages.dart';
+import 'package:capstone_vocalavida/app/style/colors.dart';
 
 class LatihanDetailController extends GetxController {
-  //TODO: Implement LatihanDetailController
+  var currentQuestionIndex = 0.obs;
+  var selectedAnswerIndex = (-1).obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  List<String> questions = [
+    "Apa yang dimaksud dengan pernapasan diafragma dalam bernyanyi? ",
+    "Mengapa pemanasan vokal penting sebelum menyanyi?",
+    "Apa itu artikulasi dalam konteks bernyanyi?",
+    "Latihan trilling bibir membantu dalam:",
+    "Apa yang dimaksud dengan pitch dalam bernyanyi?"
+  ];
+
+  List<List<String>> options = [
+    [
+      "Pernapasan menggunakan otot dada",
+      "Pernapasan yang dangkal dan cepat ",
+      "Pernapasan untuk mengontrol aliran udara",
+      "Pernapasan hanya melalui hidung"
+    ],
+    [
+      "Untuk menghafal lirik lagu",
+      "Untuk melenturkan otot-otot suara dan mencegah cedera",
+      "Untuk mempercepat tempo lagu",
+      "Untuk memperbesar volume suara"
+    ],
+    [
+      "Kecepatan bernyanyi",
+      "Kekuatan suara",
+      "Pengucapan kata-kata dengan jelas",
+      "Tinggi rendahnya nada suara"
+    ],
+    [
+      "Mengontrol pitch",
+      "Melenturkan otot-otot bibir dan wajah",
+      "Mengatur tempo",
+      " Menghafal lirik"
+    ],
+    [
+      "Volume suara yang dihasilkan",
+      "Kecepatan menyanyi",
+      "Tinggi rendahnya nada suara",
+      "Kejelasan pengucapan lirik"
+    ]
+  ];
+
+  void nextQuestion() {
+    if (currentQuestionIndex.value < questions.length - 1) {
+      currentQuestionIndex.value++;
+      selectedAnswerIndex.value = -1;
+    } else {
+      // Tampilkan dialog hasil atau navigasi ke halaman hasil
+      Get.dialog(
+        AlertDialog(
+          title: Center(
+            child: Text(
+              "Hasil",
+              style: TextStyle(
+                fontFamily: 'Exo',
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: ultramarineBlue,
+                  shape: BoxShape.circle,
+                ),
+                padding: EdgeInsets.all(8),
+                child: Icon(
+                  Icons.check,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Text(
+                  "Selamat kamu berhasil",
+                  style: TextStyle(
+                    fontFamily: 'Exo',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Text(
+                  "Kamu telah menyelesaikan semua soal",
+                  style: TextStyle(
+                    fontFamily: 'Exo',
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Get.offAllNamed(Routes.MAIN);
+                },
+                child: Text(
+                  "Selesai",
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.resolveWith<Color>((states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.red;
+                    }
+                    return ultramarineBlue;
+                  }),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void selectAnswer(int index) {
+    selectedAnswerIndex.value = index;
   }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/latihan_detail/views/latihan_detail_view.dart
+++ lib/app/modules/latihan_detail/views/latihan_detail_view.dart
@@ -1,5 +1,3 @@
-import 'package:capstone_vocalavida/app/modules/latihan/views/latihan_view.dart';
-import 'package:capstone_vocalavida/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
@@ -7,15 +5,16 @@ import 'package:capstone_vocalavida/app/style/colors.dart'; // Import the colors
 
 import '../controllers/latihan_detail_controller.dart';
 
-class LatihanDetailView extends GetView<LatihanDetailController> {
+class LatihanDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final LatihanDetailController controller =
+        Get.put(LatihanDetailController());
     double buttonWidth = MediaQuery.of(context).size.width / 2;
-    RxInt selectedButtonIndex = 0.obs;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: Text('Latihan Bernyanyi'),
         centerTitle: true,
       ),
       body: Container(
@@ -27,281 +26,121 @@ class LatihanDetailView extends GetView<LatihanDetailController> {
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: StepProgressIndicator(
-                  totalSteps: 5,
-                  currentStep: 1,
-                  size: 10,
-                  selectedColor: ultramarineBlue,
-                  unselectedColor: Colors.grey,
-                  roundedEdges: Radius.circular(5),
+          child: Obx(() {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: StepProgressIndicator(
+                    totalSteps: controller.questions.length,
+                    currentStep: controller.currentQuestionIndex.value + 1,
+                    size: 10,
+                    selectedColor: ultramarineBlue,
+                    unselectedColor: Colors.grey,
+                    roundedEdges: Radius.circular(5),
+                  ),
                 ),
-              ),
-              SizedBox(height: 40),
-              Text(
-                "Facebook Founder",
-                style: TextStyle(
-                  fontFamily: 'Exo',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+                SizedBox(height: 40),
+                Center(
+                  child: Text(
+                    controller.questions[controller.currentQuestionIndex.value],
+                    style: TextStyle(
+                      fontFamily: 'Exo',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Obx(() => Container(
-                    width: double.infinity,
-                    child: OutlinedButton(
-                      onPressed: () {
-                        selectedButtonIndex.value = 1;
-                      },
-                      style: ButtonStyle(
-                        side: MaterialStateProperty.all(
-                          BorderSide(
-                            color: selectedButtonIndex.value == 1
-                                ? ultramarineBlue
-                                : Colors.grey,
+                SizedBox(height: 20),
+                ...List.generate(
+                    controller.options[controller.currentQuestionIndex.value]
+                        .length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Container(
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        onPressed: () {
+                          controller.selectAnswer(index);
+                        },
+                        style: ButtonStyle(
+                          side: MaterialStateProperty.all(
+                            BorderSide(
+                              color:
+                                  controller.selectedAnswerIndex.value == index
+                                      ? ultramarineBlue
+                                      : Colors.grey,
+                            ),
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                           ),
                         ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        child: Text(
-                          "Mark Zuckerberg",
-                          style: TextStyle(
-                            fontFamily: 'Exo',
-                            fontSize: 20,
-                            color: selectedButtonIndex.value == 1
-                                ? ultramarineBlue
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )),
-              SizedBox(height: 10),
-              Obx(() => Container(
-                    width: double.infinity,
-                    child: OutlinedButton(
-                      onPressed: () {
-                        selectedButtonIndex.value = 2;
-                      },
-                      style: ButtonStyle(
-                        side: MaterialStateProperty.all(
-                          BorderSide(
-                            color: selectedButtonIndex.value == 2
-                                ? ultramarineBlue
-                                : Colors.grey,
-                          ),
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        child: Text(
-                          "Mark Mark",
-                          style: TextStyle(
-                            fontFamily: 'Exo',
-                            fontSize: 20,
-                            color: selectedButtonIndex.value == 2
-                                ? ultramarineBlue
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )),
-              SizedBox(height: 10),
-              Obx(() => Container(
-                    width: double.infinity,
-                    child: OutlinedButton(
-                      onPressed: () {
-                        selectedButtonIndex.value = 3;
-                      },
-                      style: ButtonStyle(
-                        side: MaterialStateProperty.all(
-                          BorderSide(
-                            color: selectedButtonIndex.value == 3
-                                ? ultramarineBlue
-                                : Colors.grey,
-                          ),
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        child: Text(
-                          "Mark Murkk",
-                          style: TextStyle(
-                            fontFamily: 'Exo',
-                            fontSize: 20,
-                            color: selectedButtonIndex.value == 3
-                                ? ultramarineBlue
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )),
-              SizedBox(height: 10),
-              Obx(() => Container(
-                    width: double.infinity,
-                    child: OutlinedButton(
-                      onPressed: () {
-                        selectedButtonIndex.value = 4;
-                      },
-                      style: ButtonStyle(
-                        side: MaterialStateProperty.all(
-                          BorderSide(
-                            color: selectedButtonIndex.value == 4
-                                ? ultramarineBlue
-                                : Colors.grey,
-                          ),
-                        ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        child: Text(
-                          "Mark Murkk",
-                          style: TextStyle(
-                            fontFamily: 'Exo',
-                            fontSize: 20,
-                            color: selectedButtonIndex.value == 4
-                                ? ultramarineBlue
-                                : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )),
-              Spacer(),
-              Center(
-                child: Container(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (dialogContext) {
-                          return AlertDialog(
-                            title: Center(
-                                child: Text(
-                              "Hasil",
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          child: Center(
+                            child: Text(
+                              controller.options[
+                                  controller.currentQuestionIndex.value][index],
                               style: TextStyle(
                                 fontFamily: 'Exo',
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
+                                fontSize: 20,
+                                color: controller.selectedAnswerIndex.value ==
+                                        index
+                                    ? ultramarineBlue
+                                    : Colors.black,
                               ),
-                            )),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: ultramarineBlue,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  padding: EdgeInsets.all(8),
-                                  child: Icon(
-                                    Icons.check,
-                                    color: Colors.white,
-                                    size: 32,
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                Text(
-                                  "Selamat kamu berhasil",
-                                  style: TextStyle(
-                                    fontFamily: 'Exo',
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                Text(
-                                  "Kamu Benar 5 dari 5",
-                                  style: TextStyle(
-                                    fontFamily: 'Exo',
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                SizedBox(height: 20),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Get.offAllNamed(Routes.MAIN);
-                                  },
-                                  child: Text(
-                                    "Selesai",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  style: ButtonStyle(
-                                    backgroundColor: MaterialStateProperty
-                                        .resolveWith<Color>((states) {
-                                      if (states
-                                          .contains(MaterialState.pressed)) {
-                                        return Colors.red;
-                                      }
-                                      return ultramarineBlue;
-                                    }),
-                                    shape: MaterialStateProperty.all(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
+                              textAlign: TextAlign.center,
                             ),
-                          );
-                        },
-                      );
-                    },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(ultramarineBlue),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12.0),
-                      child: Text(
-                        "Lanjut",
-                        style: TextStyle(
-                          fontFamily: 'Exo',
-                          fontSize: 20,
-                          color: Colors.white,
+                  );
+                }),
+                Spacer(),
+                Center(
+                  child: Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (controller.selectedAnswerIndex.value != -1) {
+                          controller.nextQuestion();
+                        }
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(ultramarineBlue),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12.0),
+                        child: Text(
+                          controller.currentQuestionIndex.value <
+                                  controller.questions.length - 1
+                              ? "Lanjut"
+                              : "Kumpul",
+                          style: TextStyle(
+                            fontFamily: 'Exo',
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            );
+          }),
         ),
       ),
     );
