e9ac1a85cb82b1d501917d8e1666695aac18e0f9
syawalamir <syawalamir24@gmail.com>
2024-06-17 21:09:19 +0700

    Menabahkan controller dan view ujian details

--- lib/app/modules/ujian_detail/controllers/ujian_detail_controller.dart
+++ lib/app/modules/ujian_detail/controllers/ujian_detail_controller.dart
@@ -1,20 +1,176 @@
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:capstone_vocalavida/app/routes/app_pages.dart';
+import 'package:capstone_vocalavida/app/style/colors.dart';
 
 class UjianDetailController extends GetxController {
-  //TODO: Implement UjianDetailController
+  var currentQuestionIndex = 0.obs;
+  var selectedAnswerIndex = (-1).obs;
+  var score = 0.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  List<String> questions = [
+    "Latihan yang melatih pengucapan vokal dan konsonan termasuk dalam...",
+    "Menggunakan dinamika dan ekspresi yang tepat dalam bernyanyi membuat penampilan menjadi...",
+    "Apa yang dimaksud dengan resonansi dalam bernyanyi?",
+    "Latihan humming (mendengung) berguna untuk...",
+    "Menggunakan variasi volume suara dalam bernyanyi disebut..."
+  ];
+
+  List<List<String>> options = [
+    [
+      "Latihan pitch",
+      "Latihan dinamika",
+      "Latihan artikulasi",
+      "Latihan pernapasan"
+    ],
+    [
+      "Lebih cepat",
+      "Lebih membosankan",
+      "Lebih hidup dan menarik",
+      "Lebih keras"
+    ],
+    [
+      "Proses mengatur tinggi rendahnya nada suara",
+      "Proses menguatkan dan memantulkan suara alami dalam tubuh",
+      "Proses menghafal lirik lagu",
+      "Proses mempercepat tempo lagu"
+    ],
+    [
+      "Menghafal lirik",
+      "Meningkatkan kontrol volume",
+      "Memperkuat resonansi dalam tubuh",
+      "Menambah kecepatan bernyanyi"
+    ],
+    ["Pitch", "Artikulasi", "Diksi", "Dinamika"]
+  ];
+
+  List<String> correctAnswers = [
+    "Latihan artikulasi",
+    "Lebih hidup dan menarik",
+    "Proses menguatkan dan memantulkan suara alami dalam tubuh",
+    "Memperkuat resonansi dalam tubuh",
+    "Dinamika"
+  ];
+
+  void nextQuestion() {
+    if (selectedAnswerIndex.value != -1) {
+      if (checkAnswer()) {
+        score++;
+      }
+
+      if (currentQuestionIndex.value < questions.length - 1) {
+        currentQuestionIndex.value++;
+        selectedAnswerIndex.value = -1;
+      } else {
+        // Tampilkan dialog hasil atau navigasi ke halaman hasil
+        Get.dialog(
+          AlertDialog(
+            title: Center(
+              child: Text(
+                score.value >= 4 ? "Hasil" : "Gagal",
+                style: TextStyle(
+                  fontFamily: 'Exo',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: ultramarineBlue,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: EdgeInsets.all(8),
+                  child: Icon(
+                    score.value >= 4 ? Icons.check : Icons.close,
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Center(
+                  child: Text(
+                    score.value >= 4
+                        ? "Selamat kamu berhasil"
+                        : "Maaf kamu belum berhasil",
+                    style: TextStyle(
+                      fontFamily: 'Exo',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Center(
+                  child: Text(
+                    score.value >= 4
+                        ? "Kamu telah menyelesaikan semua soal"
+                        : "Jawaban benar kamu kurang dari 4",
+                    style: TextStyle(
+                      fontFamily: 'Exo',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Center(
+                  child: Text(
+                    "Kamu Benar: ${score.value}/${questions.length}",
+                    style: TextStyle(
+                      fontFamily: 'Exo',
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.offAllNamed(Routes.MAIN);
+                  },
+                  child: Text(
+                    "Selesai",
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.resolveWith<Color>((states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Colors.red;
+                      }
+                      return ultramarineBlue;
+                    }),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  bool checkAnswer() {
+    return options[currentQuestionIndex.value][selectedAnswerIndex.value] ==
+        correctAnswers[currentQuestionIndex.value];
   }
 
-  @override
-  void onClose() {}
-  void increment() => count.value++;
+  void selectAnswer(int index) {
+    selectedAnswerIndex.value = index;
+  }
 }
--- lib/app/modules/ujian_detail/views/ujian_detail_view.dart
+++ lib/app/modules/ujian_detail/views/ujian_detail_view.dart
@@ -1,21 +1,144 @@
+import 'package:capstone_vocalavida/app/modules/ujian_detail/controllers/ujian_detail_controller.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
+import 'package:capstone_vocalavida/app/style/colors.dart'; // Import the colors.dart
 
-import '../controllers/ujian_detail_controller.dart';
-
-class UjianDetailView extends GetView<UjianDetailController> {
+class UjianDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final UjianDetailController controller = Get.put(UjianDetailController());
+    double buttonWidth = MediaQuery.of(context).size.width / 2;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('UjianDetailView'),
+        title: Text('Ujian Bernyanyi'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'UjianDetailView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/Background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Obx(() {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: StepProgressIndicator(
+                    totalSteps: controller.questions.length,
+                    currentStep: controller.currentQuestionIndex.value + 1,
+                    size: 10,
+                    selectedColor: ultramarineBlue,
+                    unselectedColor: Colors.grey,
+                    roundedEdges: Radius.circular(5),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Center(
+                  child: Text(
+                    controller.questions[controller.currentQuestionIndex.value],
+                    style: TextStyle(
+                      fontFamily: 'Exo',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ...List.generate(
+                    controller.options[controller.currentQuestionIndex.value]
+                        .length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Container(
+                      width: double.infinity,
+                      child: OutlinedButton(
+                        onPressed: () {
+                          controller.selectAnswer(index);
+                        },
+                        style: ButtonStyle(
+                          side: MaterialStateProperty.all(
+                            BorderSide(
+                              color:
+                                  controller.selectedAnswerIndex.value == index
+                                      ? ultramarineBlue
+                                      : Colors.grey,
+                            ),
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          child: Center(
+                            child: Text(
+                              controller.options[
+                                  controller.currentQuestionIndex.value][index],
+                              style: TextStyle(
+                                fontFamily: 'Exo',
+                                fontSize: 20,
+                                color: controller.selectedAnswerIndex.value ==
+                                        index
+                                    ? ultramarineBlue
+                                    : Colors.black,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+                Spacer(),
+                Center(
+                  child: Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (controller.selectedAnswerIndex.value != -1) {
+                          controller.nextQuestion();
+                        }
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(ultramarineBlue),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12.0),
+                        child: Text(
+                          controller.currentQuestionIndex.value <
+                                  controller.questions.length - 1
+                              ? "Lanjut"
+                              : "Kumpul",
+                          style: TextStyle(
+                            fontFamily: 'Exo',
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
         ),
       ),
     );
